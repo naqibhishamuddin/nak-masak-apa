99c3eaf26822cdbe5bb398938db7db8346246b6a
Naqib Hishamuddin <naqibhishamuddin@gmail.com>
2023-04-24 19:17:35 +0800

    feat(root): repository setup ✨

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.immediate.co.uk",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
