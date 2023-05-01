21ed0a9c6627a8525b1863dbd1c8dc50fbb08754
Naqib Hishamuddin <naqibhishamuddin@gmail.com>
2023-05-01 14:30:07 +0800

    fix(_app): update broken vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import "styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
