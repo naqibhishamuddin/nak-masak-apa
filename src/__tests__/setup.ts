885f5de268ef1dd2524795f445941b6465dd97d2
Naqib Hishamuddin <naqibhishamuddin@gmail.com>
2023-05-03 00:55:11 +0800

    test(setup.ts): remove unsused mock imports

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,19 +1,5 @@
-import { mockCustomHooks } from "./utils";
 import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
-
-/** Mock footer hooks */
-mockCustomHooks("components/footer/useFooter", { onClickContact: vi.fn() });
-
-/** Mock recipe card hooks */
-mockCustomHooks("components/recipe-card/useRecipeCard", {
-  onClickRecipe: vi.fn(),
-});
-
-/** Mock search bar hooks */
-mockCustomHooks("components/search-bar/useSearchBar", {
-  onChangeText: vi.fn(),
-});
 
 /** Mock global object */
 global.open = vi.fn();
