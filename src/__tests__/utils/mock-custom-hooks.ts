50f118d1f0b05290860ac851b16db0fc1aa67372
Naqib Hishamuddin <naqibhishamuddin@gmail.com>
2023-05-18 22:00:06 +0800

    refactor: refactor tests directory

--- src/__tests__/utils/mock-custom-hooks.ts
+++
@@ -1,5 +0,0 @@
-export const mockCustomHooks = <T extends object>(path: string, method: T,) => {
-    vi.mock(path, () => {
-        return method
-    });
-}
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
-    setupFiles: ["src/__tests__/setup.ts"],
+    setupFiles: ["tests/unit/setup.ts"],
     coverage: {
       provider: "istanbul",
       reporter: ["text", "lcov", "json-summary"],
